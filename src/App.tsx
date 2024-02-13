b97f6f0db3de22cfa52149360eefd0f586704fdf
Viktoriia <vikatori1409801@gmail.com>
2024-02-13 15:43:26 +0200

    Installed necessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
+import SingleProductPage from "./pages/SingleProductPage";
+import ProductsPage from "./pages/ProductsPage";
+import UserPage from "./pages/UserPage";
+import AdminPage from "./pages/AdminPage";
 
-function App() {
-  return <div className="App">App</div>;
-}
+//Define the routes using createBrowserRouter
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+  { path: "/products/:id", element: <SingleProductPage /> },
+  { path: "shopping-cart", element: <CartPage /> },
+  { path: "users-profile", element: <UserPage /> },
+  { path: "admin-dashboard", element: <AdminPage /> },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/components/products/ProducList.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProducList = () => {
+  return <div>ProducList</div>;
+};
+
+export default ProducList;
